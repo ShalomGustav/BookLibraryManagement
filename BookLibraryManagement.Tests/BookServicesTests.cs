8d355a5302e1f71109027e664ca55d5072857509
Максим Сергеевич Фатихов <maximvaryagov@gmail.com>
2024-11-26 07:32:24 +0300

    сделал тест

--- BookLibraryManagement.Tests/BookServicesTests.cs
+++ BookLibraryManagement.Tests/BookServicesTests.cs
@@ -1,16 +1,6 @@
 ﻿using BookLibraryManagement.Interfaces;
 using BookLibraryManagement.Models;
-using BookLibraryManagement.Repositories;
-using BookLibraryManagement.Services;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Moq;
-using Moq.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BookLibraryManagement.Tests
 {
@@ -21,27 +11,24 @@ namespace BookLibraryManagement.Tests
         {
             // Arrange: подготовка теста
             var mockBookService = new Mock<IBookServices>();
-            // Создаем мок (поддельный объект), который реализует интерфейс IBookServices.
-            // Это позволит настроить его методы для возврата тестовых данных.
 
             mockBookService.Setup(service => service.GetAllAsync(It.IsAny<CancellationToken>()))
                 .ReturnsAsync(new List<BookModel>
-                {
-                // Настраиваем метод GetAllAsync так, чтобы он возвращал список из двух книг.
-                new BookModel
-                {
-                    Id = Guid.NewGuid(), // Генерируем уникальный идентификатор
-                    Title = "Book 1", // Задаем название книги
-                    Author = new BookAuthorModel { FullName = "Author 1" } // Задаем автора книги
-                },
-                new BookModel
-                {
-                    Id = Guid.NewGuid(), // Генерируем уникальный идентификатор
-                    Title = "Book 2", // Задаем название второй книги
-                    Author = new BookAuthorModel { FullName = "Author 2" } // Задаем автора второй книги
-                }
+                {                
+                    new BookModel
+                    {
+                        Id = Guid.NewGuid(), 
+                        Title = "Book 1", 
+                        Author = new BookAuthorModel
+                        {
+                            Id = Guid.NewGuid(),
+                            FullName = "Author 1" ,
+                            Birthday = new DateTime(2010,11,9)
+                        },
+                        PublishedYear = 1995,
+                        Genre = "Genre 1",
+                    },
                 });
-            // Настройка мока завершена: теперь метод GetAllAsync всегда возвращает этот список.
 
             // Act: выполнение тестируемого метода
             var result = await mockBookService.Object.GetAllAsync(CancellationToken.None);
@@ -49,23 +36,15 @@ namespace BookLibraryManagement.Tests
             // CancellationToken.None указывает, что отмена вызова не предполагается.
 
             // Assert: проверка результатов
-            Assert.NotNull(result);
-            // Проверяем, что результат не является null.
-
-            Assert.Equal(2, result.Count);
-            // Проверяем, что список содержит ровно две книги.
-
+            Assert.NotNull(result); 
+            Assert.Single(result);
             Assert.Equal("Book 1", result[0].Title);
-            // Проверяем, что название первой книги в списке соответствует "Book 1".
+            Assert.Equal("Genre 1", result[0].Genre);
+            Assert.Equal(1995, result[0].PublishedYear);
 
+            Assert.NotNull(result[0].Author);
             Assert.Equal("Author 1", result[0].Author.FullName);
-            // Проверяем, что имя автора первой книги соответствует "Author 1".
-
-            Assert.Equal("Book 2", result[1].Title);
-            // Проверяем, что название второй книги в списке соответствует "Book 2".
-
-            Assert.Equal("Author 2", result[1].Author.FullName);
-            // Проверяем, что имя автора второй книги соответствует "Author 2".
+            Assert.Equal(new DateTime(2010,11,9), result[0].Author.Birthday);
         }
     }
 }
