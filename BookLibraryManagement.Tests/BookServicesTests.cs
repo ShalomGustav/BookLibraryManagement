c07e600aa3daf9d1ac777bfd0a1c5503f9bc9702
Максим Сергеевич Фатихов <maximvaryagov@gmail.com>
2024-11-27 19:13:24 +0300

    Вынес заполнение моделей в регион ModelsOnTests там два метода для заполнения входных параметров для сервиса

--- BookLibraryManagement.Tests/BookServicesTests.cs
+++ BookLibraryManagement.Tests/BookServicesTests.cs
@@ -12,7 +12,55 @@ namespace BookLibraryManagement.Tests
         {
             _mockBookServices = new Mock<IBookServices>();
         }
+        #region ModelsOnTests
+        private BookModel CreateTestBook(Action<BookModel> configure = null)
+        {
+            var author = new BookAuthorModel
+            {
+                Id = Guid.NewGuid(),
+                FullName = "FullName 1",
+                Birthday = new DateTime(2000, 11, 9)
+            };
 
+            var book = new BookModel
+            {
+                Id = Guid.NewGuid(),
+                Title = "Title 1",
+                Genre = "Genre 1",
+                PublishedYear = 2000,
+                Author = author,
+                AuthorId = author.Id
+            };
+
+            configure?.Invoke(book);
+            return book;
+        }
+
+        private BookAuthorModel CreateTestAuthor(Action<BookAuthorModel> configure = null)
+        {
+            var author = new BookAuthorModel
+            {
+                Id = Guid.NewGuid(),
+                FullName = "FullName 1",
+                Birthday = new DateTime(2000, 11, 9),
+            };
+
+            var book = new BookModel
+            {
+                Id = Guid.NewGuid(),
+                Title = "Title 1",
+                Genre = "Genre 1",
+                PublishedYear = 2000,
+                AuthorId = author.Id, 
+                Author = author 
+            };
+
+            author.Book = book;
+
+            configure?.Invoke(author);
+            return author;
+        }
+        #endregion
 
 
         [Fact]
@@ -20,25 +68,7 @@ namespace BookLibraryManagement.Tests
         {
             // Arrange
             _mockBookServices.Setup(x => x.GetAllAsync(It.IsAny<CancellationToken>()))
-                .ReturnsAsync(new List<BookModel>
-                {                
-                    new BookModel
-                    {
-                        Id = Guid.NewGuid(), 
-                        Title = "Book 1", 
-
-                        Author = new BookAuthorModel
-                        {
-                            Id = Guid.NewGuid(),
-                            FullName = "Author 1" ,
-                            Birthday = new DateTime(2010,11,9)
-                        },
-
-                        PublishedYear = 1995,
-                        Genre = "Genre 1",
-                    },
-                });
-
+                .ReturnsAsync(new List<BookModel> {CreateTestBook()});
             // Act
             var result = await _mockBookServices.Object.GetAllAsync(CancellationToken.None); // CancellationToken.None указывает, что отмена вызова не предполагается.
             var book = result[0];
@@ -47,14 +77,14 @@ namespace BookLibraryManagement.Tests
             Assert.Single(result);
 
             Assert.NotEqual(Guid.Empty, book.Id);
-            Assert.Equal("Book 1", book.Title);
+            Assert.Equal("Title 1", book.Title);
             Assert.Equal("Genre 1", book.Genre);
-            Assert.Equal(1995, book.PublishedYear);
+            Assert.Equal(2000, book.PublishedYear);
 
             Assert.NotNull(book.Author);
             Assert.NotEqual(Guid.Empty, book.Author.Id);
-            Assert.Equal("Author 1", book.Author.FullName);
-            Assert.Equal(new DateTime(2010,11,9), book.Author.Birthday);
+            Assert.Equal("FullName 1", book.Author.FullName);
+            Assert.Equal(new DateTime(2000, 11, 9), book.Author.Birthday);
         }
 
         [Fact]
@@ -64,21 +94,7 @@ namespace BookLibraryManagement.Tests
             _mockBookServices.Setup(x => x.GetAllAuthorAsync(It.IsAny<CancellationToken>()))
                 .ReturnsAsync(new List<BookAuthorModel> 
                 {
-                    new BookAuthorModel
-                    {
-                        Id = Guid.NewGuid(),
-                        FullName = "FullName 1",
-                        Birthday = new DateTime(2010,11,9),
-
-                        Book = new BookModel
-                        {
-                            Id = Guid.NewGuid(),
-                            Title = "Title 1",
-                            PublishedYear = 2010,
-                            Genre = "Genre 1",
-                            AuthorId = Guid.NewGuid()
-                        },
-                    }
+                    CreateTestAuthor()
                 });
 
             //Act
@@ -90,89 +106,50 @@ namespace BookLibraryManagement.Tests
 
             Assert.NotEqual(Guid.Empty, book.Id);
             Assert.Equal("FullName 1", book.FullName);
-            Assert.Equal(new DateTime(2010, 11, 9), book.Birthday);
+            Assert.Equal(new DateTime(2000, 11, 9), book.Birthday);
 
             Assert.NotNull(book.Book);
             Assert.NotEqual(Guid.Empty, book.Book.Id);
             Assert.Equal("Title 1", book.Book.Title);
-            Assert.Equal(2010, book.Book.PublishedYear);
+            Assert.Equal(2000, book.Book.PublishedYear);
             Assert.Equal("Genre 1", book.Book.Genre);
-            Assert.NotEqual(Guid.Empty, book.Book.AuthorId);
+            Assert.Equal(book.Id, book.Book.AuthorId);
         }
 
         [Fact]
         private async Task GetBookByIdAsyncTests()
         {
-            var TestId = Guid.NewGuid();
+            var testBook = CreateTestBook();
             //Arrange 
             _mockBookServices.Setup(x => x.GetBookByIdAsync(
-                It.Is<Guid>(x => x == TestId),
+                It.Is<Guid>(x => x == testBook.Id),
                 It.IsAny<CancellationToken>()))
-                .ReturnsAsync(new BookModel
-                {
-                    Id = TestId,
-                    Title = "Title 1",
-                    Genre = "Genre 1",
-                    PublishedYear = 2010,
-                    Author = new BookAuthorModel
-                    {
-                        Id = Guid.NewGuid(),
-                        FullName = "FullName 1",
-                        Birthday = new DateTime(2010, 11, 9)
-                    }
-                });
+                .ReturnsAsync(testBook);
             //Act
-            var result = await _mockBookServices.Object.GetBookByIdAsync(TestId,CancellationToken.None);
+            var result = await _mockBookServices.Object.GetBookByIdAsync(testBook.Id, CancellationToken.None);
             //Assert
             Assert.NotNull(result);
 
-            Assert.Equal(TestId, result.Id);
+            Assert.Equal(testBook.Id, result.Id);
             Assert.Equal("Title 1", result.Title);
             Assert.Equal("Genre 1", result.Genre);
-            Assert.Equal(2010, result.PublishedYear);
+            Assert.Equal(2000, result.PublishedYear);
 
             Assert.NotNull(result.Author);
-            Assert.NotEqual(Guid.Empty,result.Author.Id);
+            Assert.Equal(testBook.Author.Id, result.Author.Id);
             Assert.Equal("FullName 1", result.Author.FullName);
-            Assert.Equal(new DateTime(2010, 11, 9), result.Author.Birthday);
+            Assert.Equal(new DateTime(2000, 11, 9), result.Author.Birthday);
         }
 
         [Fact]
         private async Task CreateBookAsync()
         {
-            var bookModel = new BookModel
-            {
-                Id = Guid.NewGuid(),
-                AuthorId = Guid.NewGuid(),
-                Genre = "Genre 1",
-                PublishedYear = 2011,
-                Title = "Title 1",
-                Author = new BookAuthorModel
-                {
-                    Id = Guid.NewGuid(),
-                    Birthday = new DateTime(2011, 11, 9),
-                    FullName = "FullName 1"
-                },
-            };
-
+            var bookModel = CreateTestBook();
             //Arrange
             _mockBookServices.Setup(x => x.CreateBookAsync(
-                It.Is<BookModel>(x => x == bookModel),
+                It.Is<BookModel>(x => x.Id == bookModel.Id),
                 It.IsAny<CancellationToken>()))
-                .ReturnsAsync(new BookModel
-                {
-                    Id = bookModel.Id,
-                    AuthorId = bookModel.AuthorId,
-                    Genre = bookModel.Genre,
-                    PublishedYear = bookModel.PublishedYear,
-                    Title = bookModel.Title,
-                    Author = new BookAuthorModel
-                    {
-                        Id = bookModel.Author.Id,
-                        Birthday = bookModel.Author.Birthday,
-                        FullName = bookModel.Author.FullName
-                    }
-                });
+                .ReturnsAsync(bookModel);
 
             //Act
             var result = await _mockBookServices.Object.CreateBookAsync(bookModel, CancellationToken.None);
@@ -183,7 +160,7 @@ namespace BookLibraryManagement.Tests
             Assert.Equal(bookModel.Id, result.Id);
             Assert.Equal(bookModel.AuthorId, result.AuthorId);
             Assert.Equal("Genre 1", result.Genre);
-            Assert.Equal(2011, result.PublishedYear);
+            Assert.Equal(2000, result.PublishedYear);
             Assert.Equal("Title 1", result.Title);
 
             Assert.NotNull(result.Author);
